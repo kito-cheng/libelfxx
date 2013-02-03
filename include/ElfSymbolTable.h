3368b98e51557004f3138457e2c3146df7b371ad
Kito Cheng <kito@0xlab.org>
2013-02-03 15:56:33 +0800

    Implment symbol read for elf32

--- include/ElfSymbolTable.h
+++ include/ElfSymbolTable.h
@@ -3,23 +3,31 @@
 
 #include <ElfSymbol.h>
 #include <elf.h>
+#include <stdio.h>
 #include <map>
+#include <string>
 
 namespace libelfxx {
 
+class ElfSection;
+
 class ElfSymbolTable {
   public:
-    typedef std::map<const char *, ElfSymbol> SymbolTable;
+    typedef std::map<std::string, ElfSymbol*> SymbolTable;
     typedef SymbolTable::iterator iterator;
     typedef SymbolTable::const_iterator const_iterator;
 
-    ElfSymbolTable(Elf32_Ehdr *symtab, Elf32_Ehdr *strtab);
-    ElfSymbolTable(Elf64_Ehdr *symtab, Elf64_Ehdr *strtab);
+    ElfSymbolTable(ElfSection *symtab,
+                   ElfSection *strtab,
+                   uint8_t *rawData,
+                   bool isElf32);
 
     iterator begin();
     iterator end();
     const_iterator begin() const;
     const_iterator end() const;
+
+    void print(FILE *fp);
   private:
     SymbolTable _symtab;
 };
--- src/ElfSymbolTable.cpp
+++ src/ElfSymbolTable.cpp
@@ -1,9 +1,41 @@
 #include <ElfSymbolTable.h>
+#include <ElfSection.h>
 
 namespace libelfxx {
 
 
-ElfSymbolTable::ElfSymbolTable(Elf32_Ehdr *symtab, Elf32_Ehdr *strtab) {
+template<class Elf_Sym,
+         class Elf_Word>
+static
+ElfSymbolTable::SymbolTable initSymTab(ElfSection *symtab,
+                                       ElfSection *strtab,
+                                       uint8_t *rawData) {
+  Elf_Sym *syms = (Elf32_Sym*)(rawData + symtab->getOffset());
+  Elf_Word numSym = (symtab->getSize() / symtab->getEntsize());
+  ElfSymbolTable::SymbolTable symbolTable;
+
+  for (Elf_Word i=0;i<numSym;++i) {
+    Elf_Sym *sym = syms+i;
+    const char *symName = (const char *)(rawData +
+                                         strtab->getOffset() +
+                                         sym->st_name);
+    ElfSymbol *symbol = new ElfSymbol(symName, &syms[i]);
+    symbolTable.insert(std::make_pair(symName, symbol));
+
+  }
+  return symbolTable;
+}
+
+ElfSymbolTable::ElfSymbolTable(ElfSection *symtab,
+                               ElfSection *strtab,
+                               uint8_t *rawData,
+                               bool isElf32) {
+  if (isElf32) {
+    _symtab = initSymTab<Elf32_Sym,
+                         Elf32_Word>(symtab, strtab, rawData);
+  } else {
+    // TODO: ELF64
+  }
 }
 
 ElfSymbolTable::iterator ElfSymbolTable::begin() {
