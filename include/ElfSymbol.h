fa5eb1754aab3c765c0c9acf94efb6368a8af575
Kito Cheng <kito@0xlab.org>
2013-02-03 15:54:09 +0800

    Add print, bind and type for ElfSymbol

--- include/ElfSymbol.h
+++ include/ElfSymbol.h
@@ -3,12 +3,42 @@
 
 #include <elf.h>
 #include <stdint.h>
+#include <stdio.h>
 
 namespace libelfxx {
 
 class ElfSymbol {
   public:
+    enum class Type {
+      NOTYPE,
+      OBJECT,
+      FUNCTION,
+      SECTION,
+      FILE,
+      OTHER,
+    };
+    enum class Bind {
+      LOCAL,
+      GLOBAL,
+      WEAK,
+      OTHER,
+    };
+
     ElfSymbol(const char *name, Elf32_Sym *sym);
+
+    bool isLocal() const;
+    bool isGlobal() const;
+    bool isWeak() const;
+    Bind getBind() const;
+
+    bool isNoType() const;
+    bool isObject() const;
+    bool isFunction() const;
+    bool isSection() const;
+    bool isFile() const;
+    Type getType() const;
+
+    void print(FILE *fp);
   private:
     const char *_nameStr;
     uint32_t _name;
@@ -17,6 +47,8 @@ class ElfSymbol {
     uint16_t _shndx;
     uint64_t _value;
     uint64_t _size;
+    Bind _bind;
+    Type _type;
 };
 
 };
--- src/ElfSymbol.cpp
+++ src/ElfSymbol.cpp
@@ -1,6 +1,27 @@
 #include <ElfSymbol.h>
+#include <inttypes.h>
 
 namespace libelfxx {
+
+static ElfSymbol::Bind info2bind32 (uint8_t info) {
+  switch (ELF32_ST_BIND(info)) {
+    case STB_LOCAL:  return ElfSymbol::Bind::LOCAL;
+    case STB_GLOBAL: return ElfSymbol::Bind::GLOBAL;
+    case STB_WEAK:   return ElfSymbol::Bind::WEAK;
+    default:         return ElfSymbol::Bind::OTHER;
+  }
+}
+
+static ElfSymbol::Type info2type32 (uint8_t info) {
+  switch (ELF32_ST_TYPE(info)) {
+    case STT_NOTYPE:  return ElfSymbol::Type::NOTYPE;
+    case STT_OBJECT:  return ElfSymbol::Type::OBJECT;
+    case STT_FUNC:  return ElfSymbol::Type::FUNCTION;
+    case STT_SECTION:  return ElfSymbol::Type::SECTION;
+    case STT_FILE:  return ElfSymbol::Type::FILE;
+    default:         return ElfSymbol::Type::OTHER;
+  }
+}
 
 ElfSymbol::ElfSymbol(const char *name, Elf32_Sym *sym)
   : _nameStr(name)
@@ -10,7 +31,66 @@ ElfSymbol::ElfSymbol(const char *name, Elf32_Sym *sym)
   , _shndx(sym->st_shndx)
   , _value(sym->st_value)
   , _size(sym->st_size)
+  , _bind(info2bind32(sym->st_info))
+  , _type(info2type32(sym->st_info))
 {
+}
+
+void ElfSymbol::print(FILE *fp) {
+  fprintf(fp, "Symbol `%s`:"
+              "  st_name : %" PRIu32 "\n"
+              "  st_info : %" PRIx8 "\n"
+              "  st_other : %" PRIx8 "\n"
+              "  st_shndx : %" PRIu16 "\n"
+              "  st_value : %" PRIx64 "\n"
+              "  st_size : %" PRIx64 "\n",
+              _nameStr,
+              _name,
+              _info,
+              _other,
+              _shndx,
+              _value,
+              _size);
+}
+
+bool ElfSymbol::isLocal() const {
+  return _bind == Bind::LOCAL;
+}
+
+bool ElfSymbol::isGlobal() const {
+  return _bind == Bind::GLOBAL;
+}
+
+bool ElfSymbol::isWeak() const {
+  return _bind == Bind::WEAK;
+}
+
+ElfSymbol::Bind ElfSymbol::getBind() const {
+  return _bind;
+}
+
+bool ElfSymbol::isNoType() const {
+  return _type == Type::NOTYPE;
+}
+
+bool ElfSymbol::isObject() const {
+  return _type == Type::OBJECT;
+}
+
+bool ElfSymbol::isFunction() const {
+  return _type == Type::FUNCTION;
+}
+
+bool ElfSymbol::isSection() const {
+  return _type == Type::SECTION;
+}
+
+bool ElfSymbol::isFile() const {
+  return _type == Type::FILE;
+}
+
+ElfSymbol::Type ElfSymbol::getType() const {
+  return _type;
 }
 
 };
